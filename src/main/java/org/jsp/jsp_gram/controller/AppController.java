351cc1d78fb3d5866fcbd0f93bcf96fb73416c14
saishkulkarni <saishkulkarni7@gmail.com>
2025-01-02 12:08:50 +0530

    Registration Part - 1

--- src/main/java/org/jsp/jsp_gram/controller/AppController.java
+++ src/main/java/org/jsp/jsp_gram/controller/AppController.java
@@ -1,38 +1,47 @@
 package org.jsp.jsp_gram.controller;
 
 import org.jsp.jsp_gram.dto.User;
+import org.jsp.jsp_gram.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 
 @Controller
 public class AppController {
-	@GetMapping({"/","/login"})
+
+	@Autowired
+	UserService service;
+
+	@GetMapping({ "/", "/login" })
 	public String loadLogin() {
 		return "login.html";
 	}
 
 	@GetMapping("/register")
-	public String loadRegister(ModelMap map,User user) {
-		map.put("user", user);
-		return "register.html";
+	public String loadRegister(ModelMap map, User user) {
+		return service.loadRegister(map, user);
 	}
-	
+
 	@PostMapping("/register")
-	public String register(@Valid User user,BindingResult result) {
-		if(!user.getPassword().equals(user.getConfirmpassword()))
-			result.rejectValue("confirmpassword", "error.confirmpassword", "Passwords not Matching");
-			
-		if(result.hasErrors()) {
-			return "register.html";
-		}
-		else {
-		System.err.println(user);
-		return "redirect:https://www.youtube.com";
-		}
+	public String register(@Valid User user, BindingResult result) {
+		return service.register(user, result);
+	}
+
+	@GetMapping("/otp/{id}")
+	public String loadOtpPage(@PathVariable int id,ModelMap map) {
+		map.put("id", id);
+		return "user-otp.html";
+	}
+
+	@PostMapping("/verify-otp")
+	public String verifyOtp(@RequestParam int otp,@RequestParam int id) {
+		return service.verifyOtp(otp,id);
 	}
 }
